b9fa1a8a427038ce1873fe04ab555b20cd32a241
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-07-16 20:14:46 -0600

    Created navbar, container, object, styles

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,24 +6,36 @@ var game = {
     wins: 0,
     losses: 0,
     computerPick: "",
-    letterGuess: "",
-    numberOfGuesses: 10,
+    userPick: "",
+    numberOfGuesses: 0,
     lettersGuessed: [],
-    wordOptions: ["mountain", "peak","pinetree", "lake","tent","hike"],
+    wordOptions: ["mountain", "peak", "pinetree", "lake", "tent", "hike"],
 
-    isMatching: function(letter){
+    isMatching: function (letter) {
 
     },
-    isWordComplete: function(){
+    isWordComplete: function () {
 
+    },
+    computerPick: function () {
+        // Randomly chooses a choice from the options array. This is the Computer's guess.
+        var computerGuess = this.wordOptions[Math.floor(Math.random() * this.wordOptions.length)];
+        return computerGuess;
     }
 
 }
-document.onkeyup = function(event) {
+var computerChoice = document.getElementById("computer-word");
+// var letterChoice = document.getElementById("letters-picked");
+
+computerChoice.textContent = game.computerPick();
+
+document.onkeyup = function (event) {
 
     // Determines which key was pressed.
-    var userGuess = event.key;
+    var userGuess = event.key.toLowerCase();
 
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
-    var computerGuess = computerChoices[Math.floor(Math.random() * game.wordOptions.length)];
+    game.lettersGuessed.push(userGuess);
+    document.getElementById("letters-picked").innerHTML += userGuess; 
+    
+    // letterChoice.textContent = userGuess;
 }
