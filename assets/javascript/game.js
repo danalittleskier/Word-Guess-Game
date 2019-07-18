64c913763cb49ab96f6ce47237e8c1138502cf38
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-07-17 18:41:07 -0600

    Updating functions - underscore array, guessed word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,27 +7,52 @@ var game = {
     losses: 0,
     computerPick: "",
     userPick: "",
-    numberOfGuesses: 0,
+    numberOfGuesses: 12,
+    underscoreArray: [],
     lettersGuessed: [],
     wordOptions: ["mountain", "peak", "pinetree", "lake", "tent", "hike"],
-    isMatching: function (letter) {
-        for (let i = 0; i < this.computerPick.length; i++) {
-            const element = this.computerPick[i];
-            if(letter === element){
-                return letter;
-            } else {
-                return "_";
-            }         
-        }
-            console.log("userletter "+letter);          
-        
-    },
-    isWordComplete: function () {
 
+    isLetterMatching: function (letter) {
+        for (let i = 0; i < this.computerPick.length; i++) {
+            if(letter === this.computerPick[i]){
+                this.underscoreArray[i] = this.computerPick[i];
+            } else {
+                this.numberOfGuesses--;
+            }
+        }         
+        return this.printGuessedWord();
     },
+
+    printGuessedWord: function () {
+        var result = "";
+        for (let index = 0; index < this.underscoreArray.length; index++) {
+            result += "<span>"+this.underscoreArray[index]+"</span>&nbsp;";           
+        }
+            return result;
+    },
+
+    isWordComplete: function () {
+        if(!this.underscoreArray.includes("_")){
+            this.wins++;
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    noGuessesLeft: function (){
+        if(this.numberOfGuesses === 0){
+            return true;
+        }
+            return false;
+    },
+
     computerPick: function () {
         // Randomly chooses a choice from the options array. This is the Computer's guess.
         game.computerPick = this.wordOptions[Math.floor(Math.random() * this.wordOptions.length)];
+        for (let index = 0; index < this.computerPick.length; index++) {
+            this.underscoreArray.push("_");          
+        }
         return game.computerPick;
     }
 
@@ -42,6 +67,7 @@ document.onkeyup = function (event) {
 
     game.lettersGuessed.push(userGuess);
     document.getElementById("letters-picked").innerHTML += userGuess; 
-    document.getElementById("letters").innerHTML += game.isMatching(userGuess);
+    document.getElementById("underscores").innerHTML = game.printGuessedWord();
+    document.getElementById("letters").innerHTML = game.isLetterMatching(userGuess);
 
 }
